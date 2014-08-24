b6eed3582a6c6043fe001aeb98f983cb1db93e5a
shanujse <shanujse@gmail.com>
2014-08-24 14:27:33 +0530

    adding knowlagebase system

---
+++ NetBeansProjects/knowlagebase/src/knowlagebase/Knowlagebase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knowlagebase;
+
+/**
+ *
+ * @author Heshan
+ */
+public class Knowlagebase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
